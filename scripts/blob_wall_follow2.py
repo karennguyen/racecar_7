3c770eb050290dd7e906dacdc43c30a0190875a7
karennguyen <nguyenk@bxscience.edu>
2016-08-02 09:46:31 -0400

    brennan is potato

--- scripts/blob_wall_follow2.py
+++ scripts/blob_wall_follow2.py
@@ -10,80 +10,80 @@ from sensor_msgs.msg import LaserScan # laser scanner msgs
 from bwsi_race.msg import blob_detect
 
 class Follower():
-    e1=0
-    e2=0
-    right=True
-    death=False
-    wall=False
-    
-    def getError(self,goal,L,begin,end):
-        return (min(L[begin:end]))-goal
-        
+    e1 = 0
+    e2 = 0
+    right = True
+    death = False
+    wall = False
+
+    def getError(self, goal, L, begin, end):
+        return (min(L[begin:end])) - goal
+
     #get the angle
-    def getSteeringCmd(self,error,fullLeft,fullRight):
+    def getSteeringCmd(self, error, fullLeft, fullRight):
         Kp = .6
         Kd = .7
-        de= error-self.e2
-        self.e2=self.e1
-        self.e1=error
+        de = error - self.e2
+        self.e2 = self.e1
+        self.e1 = error
 
-        u=Kp*error+Kd*de
+        u = Kp * error + Kd * de
         return u
-    
-    def switch(self,color):
-        wall=True
-        if(color=="red"):
-            self.right=True
+
+    def switch(self, color):
+        wall = True
+        if(color == "red"):
+            self.right = True
         else:
-            self.right=False
-        
+            self.right = False
+
     #passed to the subscriber
     def laserCall(self,msg):
         if(not self.wall):
             return
         # fill out fields in ackermann steering message (to go straight)
         drive_cmd = AckermannDriveStamped()
-        
+
         if not self.right: #right
-            error=self.getError(0.5, msg.ranges, 200, 550)
-            if(error>-.5):
-                angle=self.getSteeringCmd(error, -1, 1)
+            error = self.getError(0.5, msg.ranges, 200, 550)
+            if(error > -.5):
+                angle = self.getSteeringCmd(error, -1, 1)
             else:
-                angle=1
+                angle = 1
         else: #left
-            error=self.getError(0.5, msg.ranges, 530,900)
-            if(error>-.5):
-                angle=self.getSteeringCmd(error, -1, 1)
+            error = self.getError(0.5, msg.ranges, 530,900)
+            if(error > -.5):
+                angle = self.getSteeringCmd(error, -1, 1)
             else:
-                angle=-1
-        
-        self.death=min(msg.ranges[520:560])<.2
-        drive_cmd.drive.steering_angle=angle
+                angle = -1
+
+        self.death = min(msg.ranges[520:560])<.2
+        drive_cmd.drive.steering_angle = angle
 
         if self.death:
             print "Wall follower dead"
-            drive_cmd.drive.speed=-.1
+            drive_cmd.drive.speed = -.1
         else:
             print "Angle is %f" % angle
             drive_cmd.drive.speed = 0
-    
+
         self.drive.publish(drive_cmd) # post this message
-    
+
     def blobCall(self,msg):
         '''
         msg.color    -> String
         msg.height   -> float
         msg.location -> float
         '''
-        
-        if(msg.height.data>800 or self.wall):
+
+        if (msg.height.data > 800 or self.wall):
             self.switch(msg.color)
             return
-        
+
         drive_cmd = AckermannDriveStamped()
-        
-        drive_cmd.drive.steering_angle=.5-msg.location.data
-        drive_cmd.drive.speed=1
+
+        drive_cmd.drive.steering_angle =.5 - msg.location.data
+        drive_cmd.drive.speed = 1
         self.drive.publish(drive_cmd)
 
     def shutdown(self):
@@ -91,23 +91,23 @@ class Follower():
         # always make sure to leave the robot stopped
         self.drive.publish(AckermannDriveStamped())
         rospy.sleep(1)
-    
+
     def __init__(self,bool_direction):
         print "Beginning wall follow"
         #setup the node
         rospy.init_node('wall_follower', anonymous=False)
         rospy.on_shutdown(self.shutdown)
-        self.right=bool_direction
-        
+        self.right = bool_direction
+
         # node specific topics (remap on command line or in launch file)
         self.drive = rospy.Publisher('/vesc/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=5)
-        
+
         #sets the subscriber
         rospy.Subscriber('scan', LaserScan, self.laserCall)
         rospy.Subscriber('blob_info', blob_detect,self.blobCall)
-        rospy.spin() 
+        rospy.spin()
         # always make sure to leave the robot stopped
         self.drive.publish(AckermannDriveStamped())
-        
+
 if __name__=="__main__":
 	Follower(True)
