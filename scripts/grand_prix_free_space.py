e1895828d1378976d27128214a4ef0ef5af51e74
Brennan Cain <brenn.cain@gmail.com>
2016-08-02 16:23:32 -0400

    Update grand_prix_free_space.py

--- scripts/grand_prix_free_space.py
+++ scripts/grand_prix_free_space.py
@@ -1,25 +1,76 @@
-def findLargestSpace(self, L, threshold):
-	      	center = 0
-	    	largestSpace = 0
-	    	newL = []
-	    
-		#mark all legal areas as True
-	    	for i in L:
-			newL.append(i > threshold)
-		    
-		#go through and find the largest free space
+#!/usr/bin/env python
 
-		
+# general imports for all python nodes
+import rospy
+import math
+import numpy as np
+# node specific imports
+from ackermann_msgs.msg import AckermannDriveStamped # steering messages
+from sensor_msgs.msg import LaserScan # laser scanner msgs
 
-	    	for i in range(len(newL)):
-			if(newL[i]):
-				count = 0
-		    		while newL[i]:
-					count += 1
-		        		i += 1
-					if(i>=len(newL)):
-						break
-		    		if(count > largestSpace):
-		        		largestSpace = count
-		        		center = (count / 2) + i
-return center # a point out of 1081
+
+MAX_SPEED=2.0
+
+class Follower():
+    
+    '''
+    ******************************************************************************************
+    *                                  Begin Driving stuffs                                  *
+    ******************************************************************************************
+    '''
+    
+    def findLargestSpace(self, L, threshold):
+        center = 0
+        largestSpace = 0     
+
+        for i in range(len(L)):
+            if(L[i]>threshold):
+                count = 0
+                while L[i]>threshold:
+                    count += 1
+                    i += 1
+                    if(i>=len(L)):
+                        break
+                    if(count > largestSpace):
+                        largestSpace = count
+                        center = (count / 2) + i
+        return center # a point out of 1081
+        
+        
+    '''
+    callback for the laser subscriber
+    '''
+    def laserCall(self,msg):
+        
+        #create the new message
+        drive_cmd = AckermannDriveStamped()
+        
+        drive_cmd.drive.steering_angle=(540-self.findLargestSpace(msg.ranges, 2)) /200
+        
+        drive_cmd.drive.speed=MAX_SPEED
+    
+        self.drive.publish(drive_cmd) # post this message
+      
+    
+    '''
+    *************************************************************************************************
+    *                      Constructor and initialization of instance variables                     *
+    *************************************************************************************************
+    '''
+    def __init__(self):        
+        
+        '''
+        Node setup and start
+        '''
+        rospy.init_node('grand_prix', anonymous=False)
+        self.drive = rospy.Publisher('/vesc/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=5)
+        rospy.Subscriber('scan', LaserScan, self.laserCall)
+        
+        '''
+        Leave the robot going until roscore dies, then set speed to 0
+        '''
+        rospy.spin()
+        self.drive.publish(AckermannDriveStamped())
+        
+if __name__=="__main__":
+    Follower(True)
